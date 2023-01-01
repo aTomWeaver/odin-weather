0fb5d0e3e73ce450611268bfe24eb2e23cf11e7f
aTomWeaver <atomweavermail@gmail.com>
2023-01-01 17:23:31 -0500

    basic mockup

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const KEY = "35934e2bfd811641c8956d7abbcd70bf";
 
 const getBtn = document.getElementById("get-weather");
-getBtn.addEventListener("click", getWeather);
+getBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  input = document.getElementById('loc').value
+  console.log(input)
+  getWeather(input)
+});
 
 async function getWeather(loc) {
   let response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&APPID=${KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${loc}&units=imperial&APPID=${KEY}`
   );
   const weather = await response.json();
-  console.log(weather.main.temp);
+  console.log(weather)
 }
+
