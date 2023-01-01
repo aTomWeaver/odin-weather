d68bb79f24b8caa2787117b159b14cf128474517
aTomWeaver <atomweavermail@gmail.com>
2023-01-01 14:30:53 -0500

    add get weather button

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('up and running')
+const KEY = "35934e2bfd811641c8956d7abbcd70bf";
+
+const getBtn = document.getElementById('get-weather');
+getBtn.addEventListener('click', getWeather);
+
+function getWeather(loc) {
+  console.log('testing');
+}
+
