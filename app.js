221833ccecce2ac164733bcee0af21341f414e5f
aTomWeaver <atomweavermail@gmail.com>
2023-01-01 14:44:52 -0500

    console log api call

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const KEY = "35934e2bfd811641c8956d7abbcd70bf";
 
-const getBtn = document.getElementById('get-weather');
-getBtn.addEventListener('click', getWeather);
+const getBtn = document.getElementById("get-weather");
+getBtn.addEventListener("click", getWeather);
 
-function getWeather(loc) {
-  console.log('testing');
+async function getWeather(loc) {
+  let response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&APPID=${KEY}`
+  );
+  const weather = await response.json();
+  console.log(weather.main.temp);
 }
-
