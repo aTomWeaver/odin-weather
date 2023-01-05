fec16b74c1f4b1d6de5877f0b4d23587325824a6
aTomWeaver <atomweavermail@gmail.com>
2023-01-04 19:03:40 -0500

    add React and weatherHUD component

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const KEY = "35934e2bfd811641c8956d7abbcd70bf";
 
 const getBtn = document.getElementById("get-weather");
+
 getBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const location = document.getElementById('loc')
   input = location.value
   location.value = '';
-  console.log(input)
   getWeather(input)
 });
 
@@ -16,5 +16,10 @@ async function getWeather(loc) {
   );
   const weather = await response.json();
   console.log(weather)
+  return weather;
 }
 
+async function renderWeather(loc) {
+  let weather = await getWeather(loc);
+
+}
